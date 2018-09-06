72beca3837c070bdab18a609079524e798099cd3
Julian Orth <julian.orth@icloud.com>
2018-09-06 21:53:49 +0200

    Initialize and basic Setup
    Basic setup with function body, dummy return value and look up table constant for roman digits

---
+++ convertIntToRoman.js
@@ -0,0 +1,31 @@
+/**
+ * Author: Julian Orth
+ * Date: 06.09.2018
+ *
+ */
+
+/**
+ * Converts a number from arabic to roman number system
+ * @param {number} int The first number to add.
+ * @return {string} Returns the roman equivalent to the input number as string
+ */
+function convertIntToRoman(int) {
+
+  'use strict';
+
+  const romanValuesLookUpObj = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1
+  };
+
+  var resultRoman = "";
+
+
+
+  return resultRoman;
+}
