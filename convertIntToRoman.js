91d59893e7fe21588575185ef68db305b89a7681
Julian Orth <julian.orth@icloud.com>
2018-09-07 08:48:54 +0200

    Function extension final and basic testing
    Extends Function to correctly return the input number as roman numbers. Now supports the rule that no roman letter may occur 4 times after each other, with the help of an extended lookup table.
    Also there is now basic testing for the input. Only numbers in the range of 1-3999 may be processed.

--- convertIntToRoman.js
+++ convertIntToRoman.js
@@ -5,7 +5,7 @@
  */
 
 /**
- * Converts a number from arabic to roman number system
+ * Converts a number from arabic to roman number system. Supports Numbers from 1 - 3999.
  * @param {number} int The first number to add.
  * @return {string} Returns the roman equivalent to the input number as string
  */
@@ -15,46 +15,67 @@ function convertIntToRoman(int) {
 
   const romanValuesLookUpObj = {
     M: 1000,
+    CM: 900,
     D: 500,
+    CD: 400,
     C: 100,
+    XC: 90,
     L: 50,
+    XL: 40,
     X: 10,
+    IX: 9,
     V: 5,
+    IV: 4,
     I: 1
   };
 
   var resultRoman = "";
   var romanValue, arabicValue, differenceToLookup;
   var substractionRest = int; // set to initial param
+  var testsPassed = false;
+
+  // execute some basic tests
+  int = parseInt(int);
+
+  if (int > 0 && int < 4000) {
+    testsPassed = true;
+  }
 
 
-  // loop as long as there is a rest
-  while (substractionRest != 0) {
+  if (testsPassed) {
 
-    // loop through look up object
-    for (romanValue in romanValuesLookUpObj) {
 
-      arabicValue = romanValuesLookUpObj[romanValue];
+    // loop as long as there is a rest
+    while (substractionRest != 0) {
 
-      // subtract current roman number from input number
-      differenceToLookup = substractionRest - arabicValue;
+      // loop through look up object
+      for (romanValue in romanValuesLookUpObj) {
 
-      /*
-       * if differenceToLookup = 0, input number equals a number from lookup table
-       * if differenceToLookup > 0, no exact match found, take next smaller possible roman number
-       */
-      if (differenceToLookup >= 0) {
+        arabicValue = romanValuesLookUpObj[romanValue];
 
-        // gets smaller each round
-        substractionRest = differenceToLookup;
+        // subtract current roman number from input number
+        differenceToLookup = substractionRest - arabicValue;
 
-        // add current matching letter to final string
-        resultRoman += romanValue;
-        break;
+        /*
+         * if differenceToLookup = 0, input number equals a number from lookup table
+         * if differenceToLookup > 0, no exact match found, take next smaller possible roman number
+         */
+        if (differenceToLookup >= 0) {
+
+          // gets smaller each round
+          substractionRest = differenceToLookup;
+
+          // add current matching letter to final string
+          resultRoman += romanValue;
+          break;
+        }
+
       }
 
     }
 
+  } else {
+    console.log("Fail. Please check that input is a number and between 1 - 3999.");
   }
 
   // return complete roman string
